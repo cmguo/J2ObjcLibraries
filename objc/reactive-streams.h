11a4d54ab640092b01a7c33a4e43d9ab303112be
guochunmao <guochunmao@100tal.com>
2021-09-29 18:42:48 +0800

    source.sh

--- objc/reactive-streams.h
+++
@@ -1,4 +0,0 @@
-#include "org/reactivestreams/Processor.h"
-#include "org/reactivestreams/Publisher.h"
-#include "org/reactivestreams/Subscriber.h"
-#include "org/reactivestreams/Subscription.h"
--- sources/source.sh
+++ sources/source.sh
@@ -1,9 +1,16 @@
 #/bin/bash
 
+MODULES="$*"
+if [ -z $MODULES ]
+then
+  MODULES="androidx.txt gson.txt okio.txt okhttp.txt reactivestreams.txt rxjava.txt retrofit.txt"
+fi
+
+
 mkdir -p ../classes
-for i in androidx.txt gson.txt okio.txt okhttp.txt reactivestreams.txt rxjava.txt retrofit.txt
+for i in $MODULES
 do
-  ${J2OBJC_HOME}/tools/j2objc.sh -arc -d .. -cp ${PWD}/../classes --prefixes prefixes.txt --dead-code-report deadcode.txt --external-annotation-file annotation.jaif -g @$i
+  ${J2OBJC_HOME}/tools/j2objc.sh -arc -d .. -cp jsr305-3.0.0.jar -cp ${PWD}/../classes --prefixes prefixes.txt --dead-code-report deadcode.txt --external-annotation-file annotation.jaif -g @$i
 done
 
 FIX_INCLUDE_NO_SOURCE=true ${J2OBJC_HOME}/tools/fix_include.sh ../objc ${J2OBJC_HOME}/frameworks/JRE.framework
